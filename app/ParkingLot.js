45b85e62caa96c78345fd4f0f7ef90b3a2a5ef7f
shindepriti <pritishinde34@gmail.com>
2020-04-29 15:47:57 +0530

    ADDED:Code To Check Vehicle Null Return Exception Test Become Pass

--- app/ParkingLot.js
+++ app/ParkingLot.js
@@ -8,17 +8,22 @@ class ParkingLot{
     }
     
     park=(vehicle)=>{
+        if (vehicle == null || vehicle == undefined) {
+            throw new Error("Vehicle Is Not Null Or Undefined")
+        }
         if(this.parkingLotCapacity.length == parkingLotMaximumCapacity){
-            parkingLotOwner.isFull();
-            airportSecurity.isFull();
+            parkingLotOwner.isFull()
+            airportSecurity.isFull()            
             throw new Error("Parking Lot Is Full")
         }
-        this.parkingLotCapacity.push(vehicle)
-        return true;
+        this.parkingLotCapacity.push(vehicle);
+        return true
     }
 
     unPark=(vehicle)=>{
-
+        if(vehicle == null || vehicle == undefined){
+            throw new Error("Vehicle Is Not Null Or Undefined")
+        }
         for(let car = 0;car < this.parkingLotCapacity.length;car++){
             if(this.parkingLotCapacity[car] == vehicle){
                 delete this.parkingLotCapacity[car]
